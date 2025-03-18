af63f298bb02097070be18dfa4024cd9e50e71da
Eva <anagwonyeadaeze42@gmail.com>
2025-03-18 16:14:08 +0100

    completed assignment one

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("Everything about me:\n");
+    about(32.0,7.0);
+    println!("\n");
+
+    // Without function
+    println!("My name is Eva, \nMat number: ENG2102751");
+    let newemoji:char= '\u{1F602}';
+    println!("My favourite emoji is {}", newemoji);
+    let a:f32= 32.0;
+    let b:f32 = 6.0;
+    println!("When you divide {} and {}, you get: {}", a,b,a/b);
+}
+
+fn about(a:f32, b:f32){
+    println!("Hi my name is Eva,\nMat number: ENG2102751.");
+    let emojicharacter:char= '\u{1F602}';
+    println!("My favourite emoji is the {} emoji",emojicharacter);
+    println!("When you divide {} and {}, you get: {}", a,b,a/b);
+}
+
+
+
+ 
